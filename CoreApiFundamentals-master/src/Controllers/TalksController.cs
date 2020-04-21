d13a3625f30ca140f80380aa62f4bb1d3f8919eb
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-04-21 00:23:01 -0400

    Create An Association Controller

---
+++ CoreApiFundamentals-master/src/Controllers/TalksController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/Camps/{moniker}/talks")]
+    [ApiController]
+    public class TalksController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+        private readonly LinkGenerator _linkGenerator;
+
+        public TalksController(ICampRepository repository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            _repository = repository;
+            _mapper = mapper;
+            _linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        // note that the value of moniker will be taken from the Url as specified above in the "Route" attribute
+        // http://localhost:6600/api/camps/ATL2018/talks
+        public async Task<IActionResult> GetCamps(string moniker)
+        {
+            try
+            {
+                var results = await _repository.GetTalksByMonikerAsync(moniker);
+
+                TalkModel[] models = _mapper.Map<TalkModel[]>(results);
+                return Ok(models);
+            }
+            catch (Exception e)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database Error");
+            }
+
+        }
+    }
+}
